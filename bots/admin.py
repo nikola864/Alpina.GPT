007765af6da2b73f3a267ddbd4c533322840b881
Nikola864 <kolyab89217115399@mail.ru>
2025-09-10 12:10:10 +0300

    create admin and serializers

--- bots/admin.py
+++ bots/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Bot, Scenario, Step
 
-# Register your models here.
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'description')
+
+@admin.register(Scenario)
+class ScenarioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bot', 'created_at')
+    list_filter = ('bot',)
+    search_fields = ('name',)
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('name', 'scenario', 'step_type', 'order')
+    list_filter = ('step_type', 'scenario')
