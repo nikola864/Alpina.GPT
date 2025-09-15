66e9a1fea5b90fd5f2407ac5d2d828801138da27
Nikola864 <kolyab89217115399@mail.ru>
2025-09-15 23:29:31 +0300

    create project without ai and tg

--- scenarios/views.py
+++ scenarios/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Scenario, Step
+from .serializers import ScenarioSerializer, StepSerializer
 
-# Create your views here.
+class ScenarioViewSet(viewsets.ModelViewSet):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+class StepViewSet(viewsets.ModelViewSet):
+    serializer_class = StepSerializer
+
+    def get_queryset(self):
+        scenario_id = self.kwargs.get('scenario_pk')
+        if scenario_id:
+            return Step.objects.filter(scenario_id=scenario_id)
+        return Step.objects.none()
+
